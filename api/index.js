5c72644ca9a40943705c570520b9e57cf4a7ba57
Jayabal <jayabal@heptotechnolgies.org>
2023-12-15 16:24:58 +0530

    Code push 2

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('Connected to mongodb')
+}).catch((err) =>{
+
+  console.log(err)
+});
+
+// mongoose.connect("mongodb+srv://jbarun70:jbajbajba@mernnew-v.l7ffb31.mongodb.net/mern-estatee?retryWrites=true&w=majority")
+
 
 const app=express();
-app.listen(3000,()=>{
+app.listen(3001,()=>{
   console.log('server is running on port 3000!!')
 })
